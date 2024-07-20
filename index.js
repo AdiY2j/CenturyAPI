5d766aafab0f923153e22d64239e3eaba29d2174
Adityakumar Yadav <35415282+AdiY2j@users.noreply.github.com>
2024-07-20 15:04:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const csv = require('csvtojson');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -12,7 +13,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/matches', (req, res) => {
-    csv().fromFile('demoapp_match.csv').then((jsonObj) => {
+    const csvPath = path.join(__dirname, 'demoapp_match.csv');
+    csv().fromFile(csvPath).then((jsonObj) => {
         res.status(200).send(jsonObj);
     });
 });
